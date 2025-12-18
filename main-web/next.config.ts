5c50c28fe9f6bf1fdea133899850c4a9779938bf
venisa sara <127692218+vanessasara@users.noreply.github.com>
2025-12-19 01:26:36 +0500

    Create next.config.ts

---
+++ main-web/next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
