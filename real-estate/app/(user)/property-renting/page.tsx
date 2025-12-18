51b29dd3e870681594cbe12da5e55a087167edc4
venisa sara <127692218+vanessasara@users.noreply.github.com>
2025-12-19 01:56:54 +0500

    Create page.tsx

---
+++ real-estate/app/(user)/property-renting/page.tsx
@@ -0,0 +1,14 @@
+import RentalListings from '@/components/property-renting/RentalListings'
+import RentalTerms from '@/components/property-renting/RentalTerms'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <RentalListings/>
+      <RentalTerms/>
+    </div>
+  )
+}
+
+export default page
